3bdbdca16863f1bdcdbf78f4b6f9c2c043d5b027
AlexMullen <alexmullen1991@gmail.com>
2015-12-19 20:30:57 +0000

    Removed evaluation and search packages as they should be part of the front-end application and not in the general library. Removing these results in a very streamlined library for reuse.

--- JDF/src/mullen/alex/jdf/common/evaluation/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author  Alex Mullen
- *
- * A package for containing classes for evaluating a board.
- */
-package mullen.alex.jdf.common.evaluation;
--- JDF/src/mullen/alex/jdf/common/search/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author  Alex Mullen
- *
- * A package containing classes related to move search.
- */
-package mullen.alex.jdf.common.search;
